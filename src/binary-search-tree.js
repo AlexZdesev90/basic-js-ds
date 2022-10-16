6512bbb8b9c33b151cce0c5dca33793b78ea67a9
AlexZdesev90 <zdesev-aleksandr@mail.ru>
2022-10-17 00:10:54 +0300

    feat: add binary methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,6 +7,7 @@ const { Node } = require('../extensions/list-tree.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+
   // class Node {
   //   constructor(data) {
   //     this.data = data;
@@ -14,42 +15,89 @@ class BinarySearchTree {
   //     this.right = null;
   //   }
   // }
-  
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor() {
+    this.main = null;//this.root -> coflict with method root ???
 }
+
+root() {
+    // if(this.main)return this.main
+    // if(!this.main)???
+    return this.main;
+  
+}
+
+add(data) {
+    const node =  new Node(data);
+    if (!this.main) {
+    this.main =  node;
+
+return 0;
+    }
+
+    let current=this.main;
+
+    while (true) {
+    if (node.data < current.data) {
+    if (!current.left) {
+    current.left=node;
+    // current.right=node;
+        return 0;
+    }
+        // current = current.right;
+        current=current.left;
+        } else {
+            if (!current.right) {
+                current.right=node;
+
+                return 0;
+            }
+            // current = current.left
+            current =  current.right
+
+        }
+    }
+}
+
+has(data) {
+    let current = this.main;
+    while (current ) {
+                if ((data === current.data) === true) {
+                    return true
+                } if ((data < current.data)) {
+                    current = current.left
+                } else {
+                    current = current.right
+                }
+            }
+    return false
+}
+
+find(data) {
+   //??? !!!
+}
+
+remove(data) {
+    //??? !!!
+}
+
+min() {
+    let current = this.main;
+    while (current.left) {
+    current = current.left
+    }
+    return current.data
+}
+
+max() {
+    let current = this.main;
+
+    while (current.right) {
+        current = current.right;
+    }
+    return current.data
+}
+}
+
 
 module.exports = {
   BinarySearchTree
